0e74eede107bcc3e742924256c7c898045bf8272
GuillemBalmanya <guillebalmanya@gmail.com>
2024-11-29 17:01:37 +0100

    modificacions split.py

--- split.py
+++ split.py
@@ -2,14 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def split_random(df):
-    #Ens quedem amb 500 usuaris aleatòris
     sampled_users = df['user_id'].drop_duplicates().sample(n=500, random_state=42)
     sampled_df = df[df['user_id'].isin(sampled_users)]
     train, test = train_test_split(sampled_df, test_size=0.2, random_state=42)
     return train, test
 
 def split_priority(df):
-    #Ens quedem amb els 500 Usuaris que més items han valorat
     user_counts = df.groupby('user_id').size().reset_index(name='count')
     top_users = user_counts.nlargest(500, 'count')
     filtered_df = df[df['user_id'].isin(top_users['user_id'])]
